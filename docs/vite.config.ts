be95841f4af75f600a0a798f5ab3ed5720b1e535
Phil Stenning <philstenning@hotmail.com>
2022-06-20 15:11:16 +0100

    fix(library): 🎨 tidy up code

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   if (mode === 'production') {
     return {
       plugins: [react()],
